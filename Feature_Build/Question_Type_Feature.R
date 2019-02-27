1c68a2c8d1ece21c356ff5927e5ce436afe1a1e1
Sam Taylor <42206179+samtaylor54321@users.noreply.github.com>
2019-02-27 19:30:25 +0000

    Update Question_Type_Feature.R

--- Feature_Build/Question_Type_Feature.R
+++ Feature_Build/Question_Type_Feature.R
@@ -6,19 +6,13 @@
 # Load Packages -----------------------------------------------------------
 
 library(tidyverse)
-library(tidytext)
-library(igraph)
-library(ggraph)
-
-# Load Data ---------------------------------------------------------------
-
-train <- read_csv("/Users/samtaylor/Downloads/quora-question-pairs/train.csv")
-test <- read_csv("/Users/samtaylor/Downloads/quora-question-pairs/test.csv")
 
 # Question Type Feature ---------------------------------------------------
 
-question_type_features <- train %>% mutate(id = id, qid1 = qid1,qid2 = qid2, question1 = str_to_lower(question1), question2 =  str_to_lower(question2)) %>% 
-  mutate(who_q1  = str_detect(question1, 'who'),
+question_type  <- function (.data) {
+  dplyr::select(.data, contains('id'), -contains('qid'), contains('question')) %>% 
+  dplyr::transmute(question1 = str_to_lower(question1), question2 =  str_to_lower(question2)) %>% 
+  dplyr::mutate(who_q1  = str_detect(question1, 'who'),
          what_q1 = str_detect(question1,'what'),
          when_q1 = str_detect(question1,'when'),
          where_q1 = str_detect(question1,'where'),
@@ -31,15 +25,7 @@ question_type_features <- train %>% mutate(id = id, qid1 = qid1,qid2 = qid2, que
          where_q2 = str_detect(str_to_lower(question2),'where'),
          why_q2 = str_detect(str_to_lower(question2),'why'),
          how_q2 = str_detect(str_to_lower(question2),'how'),
-         difference_between_q2 = str_detect(str_to_lower(question2),'difference between'),
-         who_question = who_q1 == who_q2,
-         what_question = what_q1 == what_q2,
-         when_question = when_q1 == when_q2,
-         where_question = where_q1 == where_q2,
-         why_question = why_q1 == why_q2,
-         how_question = how_q1 == how_q2,
-         difference_between_question = difference_between_q1 == difference_between_q2) %>% 
-  select(id, question1, question2, who_question, what_question, when_question, where_question, why_question, how_question, difference_between_question)
-
-
-View(head(question_type_features))
+         difference_between_q2 = str_detect(str_to_lower(question2),'difference between')) %>% 
+  dplyr::select(-contains('question')) %>% 
+  readr::write_csv('question_type_features.csv')
+}
