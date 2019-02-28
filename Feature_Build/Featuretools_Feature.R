099f4bcc01aae5430224e46565457127fadbde29
Sam Taylor <shane-warnie-warne@hotmail.co.uk>
2019-02-28 09:03:23 +0000

    featuretools script and dataset added

---
+++ Feature_Build/Featuretools_Feature.R
@@ -0,0 +1,19 @@
+
+library(featuretoolsR)
+library(magrittr)
+  
+ apply_ft_trans <- function (.data) {
+              dplyr::select(.data, contains('id'), -contains('qid'), contains('question')) %>% 
+              featuretoolsR::as_entityset(index = "id", entity_id = "base", id = "quora") %>% 
+              featuretoolsR::dfs(target_entity = names(es$entity_dict)[1], max_depth = 2) %>% 
+              tidy_feature_matrix(remove_nzv = T, nan_is_na = T) %>% 
+              write_csv('feature_tools_trans.csv')
+ }
+ 
+ 
+ apply_ft_trans(train)
+
+ 
+ 
+ 
+ 
